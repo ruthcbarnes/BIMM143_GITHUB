09a526d4ffe381aeb6a6d599afc158eff7ce6278
Ruth Barnes <rcbarnes@ucsd.edu>
2025-02-25 11:47:18 -0800

    one last class

---
+++ Class04Rstudio/Class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Ruth Barnes (PID A16747659)"
+#' date: "January 16, 2025"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+# Make this nicer
+plot(x, sin(x), col="green", type="l", lwd=3)
+
